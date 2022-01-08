dbf63a679e5c450acc5ee7d3bd153eb2f6a0cf9d
noamraveh <57923109+noamraveh@users.noreply.github.com>
2022-01-08 11:10:54 +0200

    Add files via upload

---
+++ server/src/main/kotlin/grpc_client/helloWorldClient.kt
@@ -0,0 +1,37 @@
+package com.kotlingrpc.demoGrpc.generated.main.grpckt.com.kotlingrpc.demoGrpc
+
+import com.kotlingrpc.demoGrpc.GreeterGrpcKt
+import com.kotlingrpc.demoGrpc.HelloRequest
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import java.io.Closeable
+import java.util.concurrent.TimeUnit
+
+class HelloWorldClient(private val channel: ManagedChannel) : Closeable {
+    private val stub: GreeterGrpcKt.GreeterCoroutineStub = GreeterGrpcKt.GreeterCoroutineStub(channel)
+
+    suspend fun greet(name: String) {
+        val request = HelloRequest.newBuilder().setName(name).build()
+        val response = stub.sayHello(request)
+        println("Received: ${response.message}")
+    }
+
+    override fun close() {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+    }
+}
+
+/**
+ * Greeter, uses first argument as name to greet if present;
+ * greets "world" otherwise.
+ */
+suspend fun main(args: Array<String>) {
+    val port = 50051
+
+    val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build()
+
+    val client = HelloWorldClient(channel)
+
+    val user = args.singleOrNull() ?: "world"
+    client.greet(user)
+}
---
+++ server/src/main/kotlin/grpc_server/helloWorldServer.kt
@@ -0,0 +1,47 @@
+package com.kotlingrpc.demoGrpc.generated.main.grpckt.com.kotlingrpc.demoGrpc
+import com.kotlingrpc.demoGrpc.GreeterGrpcKt
+import com.kotlingrpc.demoGrpc.HelloReply
+import com.kotlingrpc.demoGrpc.HelloRequest
+import io.grpc.Server
+import io.grpc.ServerBuilder
+import org.springframework.stereotype.Component
+
+class HelloWorldServer(private val port: Int) {
+    val server: Server = ServerBuilder
+        .forPort(port)
+        .addService(HelloWorldService())
+        .build()
+
+    fun start() {
+        server.start()
+        println("Server started, listening on $port")
+        Runtime.getRuntime().addShutdownHook(
+            Thread {
+                println("*** shutting down gRPC server since JVM is shutting down")
+                this@HelloWorldServer.stop()
+                println("*** server shut down")
+            }
+        )
+    }
+
+    private fun stop() {
+        server.shutdown()
+    }
+
+    fun blockUntilShutdown() {
+        server.awaitTermination()
+    }
+
+    private class HelloWorldService : GreeterGrpcKt.GreeterCoroutineImplBase() {
+        override suspend fun sayHello(request: HelloRequest) = HelloReply
+            .newBuilder()
+            .setMessage("Hello ${request.name}")
+            .build()
+    }
+}
+fun main() {
+    val port = System.getenv("PORT")?.toInt() ?: 50051
+    val server = HelloWorldServer(port)
+    server.start()
+    server.blockUntilShutdown()
+}
